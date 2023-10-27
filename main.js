7fe30ed46913c32aa8e19560cb94c8bad5f50cc1
milleninha <110179986+mileninha06@users.noreply.github.com>
2023-10-27 08:52:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play('');
+}
+
+
+document.querySelector('.tecla_pom');
+
+
+function tocaSomPom()
+    document.querySelectorAll('.tecla')
+//document.querySelector('.tecla_pom').onclik = tocaSomPom ();
+const listaDeTeclas= document.querySelector('.tecla');
